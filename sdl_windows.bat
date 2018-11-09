6461dc7dde7ceff0b845a7d9630889f6de960d03
Michael Savage <mikejsavage@gmail.com>
2018-11-09 13:26:06 +0200

    Skip the SDL shared lib on Windows

--- sdl_windows.bat
+++ sdl_windows.bat
@@ -2,11 +2,15 @@ mkdir build
 mkdir build\windows-debug
 mkdir build\windows-release
 
+set FLAGS=%FLAGS% -DSDL_SHARED=OFF
+set FLAGS=%FLAGS% -DFORCE_STATIC_VCRT=ON
+set FLAGS=%FLAGS% -DRENDER_D3D=OFF 
+
 robocopy SDL2-2.0.9 sdlbuild /E
 mkdir sdlbuild\build
 cd sdlbuild\build
 
-cmake -G "Visual Studio 14 2015 Win64" -DFORCE_STATIC_VCRT=ON -DRENDER_D3D=OFF ..
+cmake -G "Visual Studio 14 2015 Win64" %FLAGS% ..
 msbuild /maxcpucount SDL2-static.vcxproj
 msbuild /maxcpucount /p:Configuration=Release SDL2-static.vcxproj
 
