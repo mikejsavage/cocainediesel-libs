8e046dde0ceccc1dc53d2d272aa1ce58ec6a4113
Michael Savage <mikejsavage@gmail.com>
2018-11-09 19:26:19 +0200

    Copy out OpenAL windows libs

--- openal_windows.bat
+++ openal_windows.bat
@@ -13,13 +13,17 @@ robocopy openal-soft-openal-soft-1.19.1 openalbuild /E /NFL /NDL /NJH /NJS /NP
 cd openalbuild
 
 cmake -G "Visual Studio 14 2015 Win64" %FLAGS% .
-dir
 msbuild /maxcpucount ALL_BUILD.vcxproj
-msbuild /maxcpucount /p:Configuration=Release ALL_BUILD.vcxproj
-
-dir Debug
-dir Release
+msbuild /maxcpucount /p:Configuration=Release OpenAL.vcxproj
 
 cd ..
+
+cp openalbuild\Debug\OpenAL32.lib build\openal\windows-debug\openal.lib
+cp openalbuild\OpenAL.dir\Debug\OpenAL.pdb build\openal\windows-debug\openal.pdb
+cp openalbuild\Debug\common.lib build\openal\windows-debug\common.lib
+cp openalbuild\Debug\common.pdb build\openal\windows-debug\common.pdb
+
+cp openalbuild\Release\OpenAL32.lib build\openal\windows-release\openal.lib
+cp openalbuild\Release\common.lib build\openal\windows-release\openal.lib
 
 rmdir /S /Q openalbuild
