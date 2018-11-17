28cde839e9485c9c92239497196f9250860005e9
Michael Savage <mikejsavage@gmail.com>
2018-11-17 14:04:04 +0200

    Don't build a debug librocket on Windows because it's 264MB

--- librocket_windows.bat
+++ librocket_windows.bat
@@ -4,16 +4,15 @@ mkdir build\librocket\windows-debug
 mkdir build\librocket\windows-release
 
 set CXXFLAGS=/MP /I..\..\Include /DROCKET_STATIC_LIB /EHsc
-set DEBUGCXXFLAGS=%CXXFLAGS% /MDd /Z7 /Zo
 set RELEASECXXFLAGS=%CXXFLAGS% /MD /O2
 
 robocopy libRocket librocketbuild /E /NFL /NDL /NJH /NJS /NP
 cd librocketbuild
 
 cd Source\Core
-cl /c %DEBUGCXXFLAGS% *.cpp
+cl /c %RELEASECXXFLAGS% *.cpp
 cd ..\Controls
-cl /c %DEBUGCXXFLAGS% *.cpp
+cl /c %RELEASECXXFLAGS% *.cpp
 cd ..\..
 
 lib -OUT:rocket.lib Source\Core\*.obj Source\Controls\*.obj
@@ -21,22 +20,6 @@ lib -OUT:rocket.lib Source\Core\*.obj Source\Controls\*.obj
 cd ..
 
 copy librocketbuild\rocket.lib build\librocket\windows-debug
-
-rmdir /S /Q librocketbuild
-
-robocopy libRocket librocketbuild /E /NFL /NDL /NJH /NJS /NP
-cd librocketbuild
-
-cd Source\Core
-cl /c %RELEASECXXFLAGS% *.cpp
-cd ..\Controls
-cl /c %RELEASECXXFLAGS% *.cpp
-cd ..\..
-
-lib -OUT:rocket.lib Source\Core\*.obj Source\Controls\*.obj
-
-cd ..
-
 copy librocketbuild\rocket.lib build\librocket\windows-release
 
 rmdir /S /Q librocketbuild
