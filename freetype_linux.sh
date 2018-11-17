98488e95b2744b93444edeefe2960bc518bebf82
Michael Savage <mikejsavage@gmail.com>
2018-11-17 11:13:09 +0200

    Linux freetype/librocket build scripts

---
+++ freetype_linux.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+mkdir -p build/freetype/linux-debug
+mkdir -p build/freetype/linux-release
+
+flags="\
+-DBUILD_SHARED_LIBS=OFF
+-DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz=TRUE \
+-DCMAKE_DISABLE_FIND_PACKAGE_BZip2=TRUE \
+-DCMAKE_DISABLE_FIND_PACKAGE_PNG=TRUE \
+-DCMAKE_DISABLE_FIND_PACKAGE_ZLIB=TRUE"
+
+cp -r freetype-2.9.1 freetypebuild
+cd freetypebuild
+
+mkdir debugbuild
+cd debugbuild
+cmake -DCMAKE_BUILD_TYPE=Debug $flags ..
+make -j$(nproc --all)
+cd ..
+
+mkdir releasebuild
+cd releasebuild
+cmake -DCMAKE_BUILD_TYPE=Release $flags ..
+make -j$(nproc --all)
+cd ..
+
+cd ..
+
+cp freetypebuild/debugbuild/libfreetyped.a build/freetype/linux-debug/libfreetype.a
+cp freetypebuild/releasebuild/libfreetype.a build/freetype/linux-release
+
+cp -r freetype-2.9.1/include/* build/freetype
+cp -r freetypebuild/debugbuild/include/* build/freetype
+
+rm -r freetypebuild
---
+++ librocket_linux.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+mkdir -p build/librocket/linux-debug
+mkdir -p build/librocket/linux-release
+
+cp -r libRocket librocketbuild
+cd librocketbuild
+
+srcs="Source/Core/*.cpp Source/Controls/*.cpp"
+debugobjs=""
+releaseobjs=""
+
+CXXFLAGS="-IInclude"
+DEBUGCXXFLAGS="$CXXFLAGS -g"
+RELEASECXXFLAGS="$CXXFLAGS -O2"
+
+for f in $srcs; do
+	g++ -c $DEBUGCXXFLAGS -o "$f.od" "$f"
+	debugobjs+="$f.od "
+done
+
+ar rs librocketdebug.a $debugobjs
+
+for f in $srcs; do
+	g++ -c $RELEASECXXFLAGS -o "$f.o" "$f"
+	debugobjs+="$f.o "
+done
+
+ar rs librocket.a $releaseobjs
+
+cd ..
+
+cp librocketbuild/librocketdebug.a build/librocket/linux-debug/librocket.a
+cp librocketbuild/librocket.a build/librocket/linux-release/librocket.a
+
+# rm -r librocketbuild
+
+cp -r libRocket/Include build/librocket
