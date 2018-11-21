e34e1c28a5971cdb56c5243b1c2e6afd291d0629
Michael Savage <mikejsavage@gmail.com>
2018-11-21 14:42:00 +0200

    .

--- zstd_windows.bat
+++ zstd_windows.bat
@@ -9,13 +9,13 @@ set FLAGS=%FLAGS% -DZSTD_BUILD_SHARED=OFF
 REM set FLAGS=%FLAGS% -DZSTD_USE_STATIC_RUNTIME=ON
 
 robocopy zstd-1.3.7 zstdbuild /E /NFL /NDL /NJH /NJS /NP
-cd zstdbuild
+cd zstdbuild\build\cmake
 
 cmake -G "Visual Studio 14 2015 Win64" %FLAGS% ..
 msbuild /maxcpucount ALL_BUILD.vcxproj
 msbuild /maxcpucount /p:Configuration=Release ALL_BUILD.vcxproj
 
-cd ..
+cd ..\..\..
 
 copy zstdbuild\build\Debug\zstd_staticd.lib build\zstd\windows-debug\zstd.lib
 copy zstdbuild\build\zstd_static.dir\Debug\zstd_static.pdb build\zstd\windows-debug\zstd.lib
