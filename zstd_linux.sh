8d2508dd034d7105f764b96ce2332c95f238fa10
Michael Savage <mikejsavage@gmail.com>
2018-11-21 13:47:08 +0200

    Add zstd build scripts

---
+++ zstd_linux.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+set -e
+
+mkdir -p build/zstd/linux-debug
+mkdir -p build/zstd/linux-release
+
+cp -r zstd-1.3.7 zstdbuild
+cd zstdbuild/lib
+make -j$(nproc --all) lib CFLAGS=-g
+cd ../..
+
+cp zstdbuild/lib/libzstd.a build/zstd/linux-debug
+
+rm -r zstdbuild
+
+cp -r zstd-1.3.7 zstdbuild
+cd zstdbuild/lib
+make -j$(nproc --all) lib-release
+cd ../..
+
+cp zstdbuild/lib/libzstd.a build/zstd/linux-release
+
+rm -r zstdbuild
---
+++ zstd_windows.bat
@@ -0,0 +1,25 @@
+mkdir build
+mkdir build\zstd
+mkdir build\zstd\windows-debug
+mkdir build\zstd\windows-release
+
+set FLAGS=%FLAGS% -DZSTD_MULTITHREAD_SUPPORT=OFF
+set FLAGS=%FLAGS% -DZSTD_BUILD_PROGRAMS=OFF
+set FLAGS=%FLAGS% -DZSTD_BUILD_SHARED=OFF
+REM set FLAGS=%FLAGS% -DZSTD_USE_STATIC_RUNTIME=ON
+
+robocopy zstd-1.3.7 zstdbuild /E /NFL /NDL /NJH /NJS /NP
+cd zstdbuild
+
+cmake -G "Visual Studio 14 2015 Win64" %FLAGS% ..
+msbuild /maxcpucount ALL_BUILD.vcxproj
+msbuild /maxcpucount /p:Configuration=Release ALL_BUILD.vcxproj
+
+cd ..
+
+copy zstdbuild\build\Debug\zstd_staticd.lib build\zstd\windows-debug\zstd.lib
+copy zstdbuild\build\zstd_static.dir\Debug\zstd_static.pdb build\zstd\windows-debug\zstd.lib
+
+copy zstdbuild\build\Release\zstd_static.lib build\zstd\windows-release\zstd.lib
+
+rmdir /S /Q zstdbuild
