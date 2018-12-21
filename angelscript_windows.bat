ff2f2e6f89d4c809465b69853670e997cd81bd5a
Michael Savage <mikejsavage@gmail.com>
2018-12-21 04:27:53 +0200

    Really fix angelscript_windows

--- angelscript_windows.bat
+++ angelscript_windows.bat
@@ -1,7 +1,7 @@
 mkdir build
-mkdir build\angelscrpt
-mkdir build\angelscrpt\windows-debug
-mkdir build\angelscrpt\windows-release
+mkdir build\angelscript
+mkdir build\angelscript\windows-debug
+mkdir build\angelscript\windows-release
 
 robocopy angelscript-2.32.0 angelscriptbuild /E /NFL /NDL /NJH /NJS /NP
 cd angelscriptbuild\angelscript\projects\cmake
@@ -12,6 +12,7 @@ msbuild /maxcpucount /p:Configuration=Release ALL_BUILD.vcxproj
 
 cd ..\..\..\..
 
+cd   angelscriptbuild\angelscript\projects\cmake
 copy angelscriptbuild\angelscript\projects\cmake\Debug\angelscriptd.lib build\angelscript\windows-debug\angelscript.lib
 
 copy angelscriptbuild\angelscript\projects\cmake\Release\angelscript.lib build\angelscript\windows-release
