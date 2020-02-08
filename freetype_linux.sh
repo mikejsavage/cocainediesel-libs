a906b11c527e740b61b8720b31be5b8410d25204
Michael Savage <mikejsavage@gmail.com>
2020-02-09 01:41:11 +0200

    -SDL +GLFW

--- freetype_linux.sh
+++ freetype_linux.sh
@@ -6,7 +6,7 @@ mkdir -p build/freetype/linux-debug
 mkdir -p build/freetype/linux-release
 
 flags="\
--DBUILD_SHARED_LIBS=OFF
+-DBUILD_SHARED_LIBS=OFF \
 -DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz=TRUE \
 -DCMAKE_DISABLE_FIND_PACKAGE_BZip2=TRUE \
 -DCMAKE_DISABLE_FIND_PACKAGE_PNG=TRUE \
---
+++ glfw_windows.bat
@@ -0,0 +1,27 @@
+mkdir build
+mkdir build\glfw3
+mkdir build\glfw3\windows-debug
+mkdir build\glfw3\windows-release
+
+set FLAGS=
+set FLAGS=%FLAGS% -DGLFW_BUILD_EXAMPLES=OFF
+set FLAGS=%FLAGS% -DGLFW_BUILD_TESTS=OFF
+set FLAGS=%FLAGS% -DGLFW_BUILD_DOCS=OFF
+set FLAGS=%FLAGS% -DUSE_MSVC_RUNTIME_LIBRARY_DLL=OFF
+
+robocopy glfw-3.3.2 glfwbuild /E /NFL /NDL /NJH /NJS /NP
+mkdir glfwbuild\build
+cd glfwbuild\build
+
+cmake -G "Visual Studio 16 2019" -A x64 %FLAGS% ..
+msbuild /maxcpucount ALL_BUILD.vcxproj
+msbuild /maxcpucount /p:Configuration=Release ALL_BUILD.vcxproj
+
+cd ..\..
+
+copy glfwbuild\build\src\Debug\glfw3.lib build\glfw3\windows-debug
+copy glfwbuild\build\src\glfw.dir\Debug\glfw.pdb build\glfw3\windows-debug
+
+copy glfwbuild\build\src\Release\glfw3.lib build\glfw3\windows-release
+
+rmdir /S /Q glfwbuild
