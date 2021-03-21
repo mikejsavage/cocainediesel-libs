7402e407aa962606331306f271a3b76755c5c149
Michael Savage <mikejsavage@gmail.com>
2021-03-21 15:05:48 +0200

    Add glad 0.1.34

---
+++ glad_linux.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+extensions="GL_AMD_debug_output,GL_EXT_texture_compression_s3tc,GL_EXT_texture_filter_anisotropic,GL_EXT_texture_sRGB,GL_EXT_texture_sRGB_decode,GL_KHR_debug"
+
+mkdir -p build/glad
+
+cd glad-0.1.34
+python3 -m glad --profile="core" --api="gl=3.3" --generator="c" --spec="gl" --extensions="$extensions" --reproducible --out-path .
+
+mkdir -p output
+mv src/glad.c output/glad.cpp
+mv include/glad/glad.h output
+mv include/KHR/khrplatform.h output
+
+sed -i "s#<glad/glad.h>#\"glad.h\"#" output/glad.cpp
+sed -i "s#<KHR/khrplatform.h>#\"khrplatform.h\"#" output/glad.h
+
+cp output/* ../build/glad
+
+rm -r src include output
