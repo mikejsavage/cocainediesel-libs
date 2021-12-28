be5516b29adb672e0593042e0912d806a33841e4
Michael Savage <mikejsavage@gmail.com>
2021-12-28 16:59:52 +0000

    OpenGL 4.3 + extensions

--- glad_linux.sh
+++ glad_linux.sh
@@ -1,11 +1,19 @@
 #! /bin/sh
 
-extensions="GL_AMD_debug_output,GL_EXT_texture_compression_s3tc,GL_EXT_texture_filter_anisotropic,GL_EXT_texture_sRGB,GL_EXT_texture_sRGB_decode,GL_KHR_debug,GL_NVX_gpu_memory_info"
+extensions=""
+extensions="$extensions,GL_EXT_texture_compression_s3tc"
+extensions="$extensions,GL_EXT_texture_filter_anisotropic"
+extensions="$extensions,GL_EXT_texture_sRGB"
+extensions="$extensions,GL_EXT_texture_sRGB_decode"
+extensions="$extensions,GL_ARB_buffer_storage"
+extensions="$extensions,GL_ARB_clip_control"
+extensions="$extensions,GL_ARB_direct_state_access"
+extensions="$extensions,GL_NVX_gpu_memory_info"
 
 mkdir -p build/glad
 
 cd glad-0.1.34
-python3 -m glad --profile="core" --api="gl=3.3" --generator="c" --spec="gl" --extensions="$extensions" --reproducible --out-path .
+python3 -m glad --profile="core" --api="gl=4.3" --generator="c" --spec="gl" --extensions="$extensions" --reproducible --out-path .
 
 mkdir -p output
 mv src/glad.c output/glad.cpp
