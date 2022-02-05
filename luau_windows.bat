522a4ea8095b54422f0f9acd8750fa15bd7b43cc
Michael Savage <mikejsavage@gmail.com>
2022-02-06 00:05:19 +0200

    Add Luau Windows build

---
+++ luau_windows.bat
@@ -0,0 +1,27 @@
+mkdir build
+mkdir build\luau
+mkdir build\luau\windows-debug
+mkdir build\luau\windows-release
+
+set FLAGS=
+set FLAGS=%FLAGS% -DLUAU_BUILD_CLI=OFF
+set FLAGS=%FLAGS% -DLUAU_BUILD_TESTS=OFF
+set FLAGS=%FLAGS% -DLUAU_STATIC_CRT=ON
+
+robocopy luau-0.512 luaubuild /E /NFL /NDL /NJH /NJS /NP
+cd luaubuild
+
+cmake -G "Visual Studio 16 2019" -A x64 %FLAGS% .
+
+msbuild /maxcpucount Luau.Compiler.vcxproj
+msbuild /maxcpucount Luau.VM.vcxproj
+msbuild /maxcpucount /p:Configuration=Release Luau.Compiler.vcxproj
+msbuild /maxcpucount /p:Configuration=Release Luau.VM.vcxproj
+
+cd ..
+
+copy luaubuild\Debug\*.lib build\luau\windows-debug\
+copy luaubuild\Debug\*.pdb build\luau\windows-debug\
+copy luaubuild\Release\*.lib build\luau\windows-release\
+
+rmdir /S /Q luaubuild
