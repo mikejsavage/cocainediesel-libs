2e80d7fdb3293b25bcf36d71b6d64388a66bff9e
Michael Savage <mike@mikejsavage.co.uk>
2022-04-13 00:58:09 +0300

    GL 4.5

--- glad_linux.sh
+++ glad_linux.sh
@@ -5,16 +5,12 @@ extensions="$extensions,GL_EXT_texture_compression_s3tc"
 extensions="$extensions,GL_EXT_texture_filter_anisotropic"
 extensions="$extensions,GL_EXT_texture_sRGB"
 extensions="$extensions,GL_EXT_texture_sRGB_decode"
-extensions="$extensions,GL_ARB_buffer_storage"
-extensions="$extensions,GL_ARB_clip_control"
-extensions="$extensions,GL_ARB_direct_state_access"
-extensions="$extensions,GL_EXT_direct_state_access"
 extensions="$extensions,GL_NVX_gpu_memory_info"
 
 mkdir -p build/glad
 
 cd glad-0.1.34
-python3 -m glad --profile="core" --api="gl=4.3" --generator="c" --spec="gl" --extensions="$extensions" --reproducible --out-path .
+python3 -m glad --profile="core" --api="gl=4.5" --generator="c" --spec="gl" --extensions="$extensions" --reproducible --out-path .
 
 mkdir -p output
 mv src/glad.c output/glad.cpp
