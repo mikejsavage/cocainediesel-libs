d85b8dc939facc81752f3080070aa165c135d312
Michael Savage <mike@mikejsavage.co.uk>
2022-04-23 12:05:09 +0300

    glfw 3.3.5 -> 3.3.7

--- glfw_linux.sh
+++ glfw_linux.sh
@@ -10,7 +10,7 @@ flags="\
 -DGLFW_BUILD_TESTS=OFF \
 -DGLFW_BUILD_DOCS=OFF"
 
-cp -r glfw-3.3.5 glfwbuild
+cp -r glfw-3.3.7 glfwbuild
 cd glfwbuild
 
 mkdir debugbuild
@@ -30,6 +30,6 @@ cd ..
 cp glfwbuild/debugbuild/src/libglfw3.a build/glfw3/linux-debug
 cp glfwbuild/releasebuild/src/libglfw3.a build/glfw3/linux-release
 
-cp -r glfw-3.3.5/include/* build/glfw3
+cp -r glfw-3.3.7/include/* build/glfw3
 
 rm -r glfwbuild
