951e719f0d778dcbad995b85c167cb71b03cba58
Michael Savage <mike@mikejsavage.co.uk>
2022-07-14 23:55:33 +0300

    Fix Discord Windows build, add discord to appveyor specs

--- discord_windows.bat
+++ discord_windows.bat
@@ -12,8 +12,8 @@ msbuild /maxcpucount /p:Configuration=Release discord.vcxproj
 
 cd ..
 
-copy discord\Debug\discord.lib build\discord\windows-debug
-copy discord\Debug\discord.pdb build\discord\windows-debug
-copy discord\Release\discord.lib build\discord\windows-release
+copy discordbuild\Debug\discord.lib build\discord\windows-debug
+copy discordbuild\Debug\discord.pdb build\discord\windows-debug
+copy discordbuild\Release\discord.lib build\discord\windows-release
 
 rmdir /S /Q discordbuild
