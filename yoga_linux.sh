032597682f32339d3cc73bef9918a08f607a5c49
Michael Savage <mike@mikejsavage.co.uk>
2024-01-14 15:06:06 +0200

    Deshittify the yoga build so Github doesn't spam us with idiotic dependabot PRs

--- yoga_linux.sh
+++ yoga_linux.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "113b562ccae72f737c3c6016c0129000b6031718b21d662d3032eef87f0264c6 yoga-2.0.1/CMakeLists.txt" | sha256sum -c
+
 mkdir -p build/yoga/linux-debug
 mkdir -p build/yoga/linux-release
 
@@ -9,14 +11,14 @@ cp -r yoga-2.0.1 yogabuild
 cd yogabuild
 
 cmake -Bdebugbuild
-cmake --build debugbuild --target yogacore --config Debug --parallel $(nproc --all)
+cmake --build debugbuild --config Debug --parallel $(nproc --all)
 cmake -Breleasebuild
-cmake --build releasebuild --target yogacore --config Release --parallel $(nproc --all)
+cmake --build releasebuild --config Release --parallel $(nproc --all)
 
 cd ..
 
-cp yogabuild/debugbuild/yoga/libyogacore.a build/yoga/linux-debug
-cp yogabuild/releasebuild/yoga/libyogacore.a build/yoga/linux-release
+cp yogabuild/debugbuild/libyoga.a build/yoga/linux-debug
+cp yogabuild/releasebuild/libyoga.a build/yoga/linux-release
 
 rm -r yogabuild
 
