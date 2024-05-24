fe80b6f13002c14ef7333eb059d25eda7ab77f05
Michael Savage <mike@mikejsavage.co.uk>
2024-05-24 10:18:17 +0300

    Use ziglang.org/download because ziglang.org/builds 403s old versions

--- zig-cmake/download_zig.sh
+++ zig-cmake/download_zig.sh
@@ -10,7 +10,7 @@ if [ -e "zig-$version/zig" ]; then
 	exit
 fi
 
-wget --continue "https://ziglang.org/builds/zig-linux-x86_64-$version.tar.xz"
+wget --continue "https://ziglang.org/download/$version/zig-linux-x86_64-$version.tar.xz"
 echo "2d00e789fec4f71790a6e7bf83ff91d564943c5ee843c5fd966efc474b423047 zig-linux-x86_64-$version.tar.xz" | sha256sum -c
 tar --touch --no-same-owner -xf "zig-linux-x86_64-$version.tar.xz"
 rm "zig-linux-x86_64-$version.tar.xz"
